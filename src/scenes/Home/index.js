1c087c2219297dc504613d96cdbb8268376ef19c
kudoro <himanshu@prodios.com>
2018-05-27 21:42:57 +0530

    implement get user current location and integrate weather api

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,98 +1,163 @@
 import React from 'react';
-import { StyleSheet, View, Text, ImageBackground, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground, Dimensions, Platform } from 'react-native';
 import Swiper from 'react-native-swiper';
-import { LinearGradient } from 'expo';
+import { LinearGradient, Permissions, Location, Constants } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 
 const { width: DEVICE_WIDTH } = Dimensions.get('window');
+const WEATHER_API_KEY = '69f95bb4c7a6daf7c80f11a1f3b9c3d1';
 
-const Home = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      style={styles.background}
-      source={require('../../assets/images/home-background.png')}
-      resizeMode="cover"
-    >
-      <LinearGradient style={styles.background} colors={['rgba(0, 0, 0, 0.5)', 'transparent']}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>Bengluru, Karnataka</Text>
-          <Ionicons name="ios-sunny-outline" size={60} color="#fff" />
-          <View style={styles.temperatureContainer}>
-            <Text style={styles.temperatureText}>26</Text>
-            <Text style={styles.degreetext}>o</Text>
-          </View>
-        </View>
-      </LinearGradient>
-    </ImageBackground>
+class Home extends React.Component {
+  state = {
+    location: null,
+    errorMessage: null,
+    weatherData: null,
+  };
 
-    <Swiper activeDotColor="rgba(0, 0, 0, 0.87)" horizontal>
-      <View style={styles.slide}>
-        <View style={styles.slideContent}>
-          <Text style={styles.slideTitle}>SUN</Text>
-          <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
-          <View style={styles.temperatureContainer}>
-            <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
-            <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+  componentDidMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      // eslint-disable-next-line react/no-did-mount-set-state
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this.getLocationAsync();
+    }
+  }
+
+  getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (status === 'granted') {
+      const location = await Location.getCurrentPositionAsync({});
+      this.setState(
+        {
+          location: {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          },
+        },
+        () => {
+          this.getWeatherUsingLatlng();
+        },
+      );
+    } else {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+  };
+
+  getWeatherUsingLatlng = async () => {
+    await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${this.state.location.latitude}&lon=${
+      this.state.location.longitude
+    }&appid=${WEATHER_API_KEY}&units=metric`)
+      .then(data => data.json())
+      .then((result) => {
+        this.setState({
+          weatherData: result,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.background}
+          source={require('../../assets/images/home-background.png')}
+          resizeMode="cover"
+        >
+          <LinearGradient style={styles.background} colors={['rgba(0, 0, 0, 0.5)', 'transparent']}>
+            <View style={styles.titleContainer}>
+              {this.state.weatherData && !this.state.errorMessage ? (
+                [
+                  <Text key="title" style={styles.title}>
+                    {`${this.state.weatherData.city.name}, ${this.state.weatherData.city.country}`}
+                  </Text>,
+                  <Ionicons key="icon" name="ios-sunny-outline" size={60} color="#fff" />,
+                  <View key="temperature" style={styles.temperatureContainer}>
+                    <Text style={styles.temperatureText}>{this.state.weatherData.list[0].main.temp}</Text>
+                    <Text style={styles.degreetext}>o</Text>
+                  </View>,
+                ]
+              ) : (
+                <Text style={styles.errorMessage}>{this.state.errorMessage}</Text>
+              )}
+            </View>
+          </LinearGradient>
+        </ImageBackground>
+
+        <Swiper activeDotColor="rgba(0, 0, 0, 0.87)" horizontal>
+          <View style={styles.slide}>
+            <View style={styles.slideContent}>
+              <Text style={styles.slideTitle}>SUN</Text>
+              <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
+              <View style={styles.temperatureContainer}>
+                <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
+                <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+              </View>
+            </View>
+            <View style={styles.slideContent}>
+              <Text style={[styles.slideTitle, { color: 'rgba(0, 0, 0, 0.87)' }]}>MON</Text>
+              <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.87)" />
+              <View style={styles.temperatureContainer}>
+                <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.87)' }}>26</Text>
+                <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.87)' }}>o</Text>
+              </View>
+            </View>
+            <View style={styles.slideContent}>
+              <Text style={styles.slideTitle}>TUE</Text>
+              <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
+              <View style={styles.temperatureContainer}>
+                <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
+                <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+              </View>
+            </View>
           </View>
-        </View>
-        <View style={styles.slideContent}>
-          <Text style={[styles.slideTitle, { color: 'rgba(0, 0, 0, 0.87)' }]}>MON</Text>
-          <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.87)" />
-          <View style={styles.temperatureContainer}>
-            <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.87)' }}>26</Text>
-            <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.87)' }}>o</Text>
+
+          <View style={styles.slide}>
+            <View style={styles.slideContent}>
+              <Text style={styles.slideTitle}>WED</Text>
+              <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
+              <View style={styles.temperatureContainer}>
+                <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
+                <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+              </View>
+            </View>
+            <View style={styles.slideContent}>
+              <Text style={styles.slideTitle}>THU</Text>
+              <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
+              <View style={styles.temperatureContainer}>
+                <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
+                <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+              </View>
+            </View>
+            <View style={styles.slideContent}>
+              <Text style={styles.slideTitle}>FRI</Text>
+              <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
+              <View style={styles.temperatureContainer}>
+                <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
+                <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+              </View>
+            </View>
           </View>
-        </View>
-        <View style={styles.slideContent}>
-          <Text style={styles.slideTitle}>TUE</Text>
-          <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
-          <View style={styles.temperatureContainer}>
-            <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
-            <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+
+          <View style={styles.slide}>
+            <View style={styles.slideContent}>
+              <Text style={styles.slideTitle}>SAT</Text>
+              <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
+              <View style={styles.temperatureContainer}>
+                <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
+                <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
+              </View>
+            </View>
           </View>
-        </View>
+        </Swiper>
       </View>
-
-      <View style={styles.slide}>
-        <View style={styles.slideContent}>
-          <Text style={styles.slideTitle}>WED</Text>
-          <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
-          <View style={styles.temperatureContainer}>
-            <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
-            <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
-          </View>
-        </View>
-        <View style={styles.slideContent}>
-          <Text style={styles.slideTitle}>THU</Text>
-          <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
-          <View style={styles.temperatureContainer}>
-            <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
-            <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
-          </View>
-        </View>
-        <View style={styles.slideContent}>
-          <Text style={styles.slideTitle}>FRI</Text>
-          <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
-          <View style={styles.temperatureContainer}>
-            <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
-            <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
-          </View>
-        </View>
-      </View>
-
-      <View style={styles.slide}>
-        <View style={styles.slideContent}>
-          <Text style={styles.slideTitle}>SAT</Text>
-          <Ionicons name="ios-sunny-outline" size={35} color="rgba(0, 0, 0, 0.54)" />
-          <View style={styles.temperatureContainer}>
-            <Text style={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.51)' }}>26</Text>
-            <Text style={{ fontSize: 10, color: 'rgba(0, 0, 0, 0.51)' }}>o</Text>
-          </View>
-        </View>
-      </View>
-    </Swiper>
-  </View>
-);
+    );
+  }
+}
 
 export default Home;
 
@@ -142,5 +207,10 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     fontSize: 24,
     color: 'rgba(0, 0, 0, 0.51)',
+  },
+  errorMessage: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
   },
 });
